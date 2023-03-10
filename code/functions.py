3038d369e9e1212c5c4aa932b6a7df72b396fe56
Turtilla <98593377+Turtilla@users.noreply.github.com>
2023-03-10 18:30:21 +0100

    Update functions.py

--- code/functions.py
+++ code/functions.py
@@ -4,13 +4,14 @@ import sklearn.metrics
 import matplotlib.pyplot as plt
 import time
 
-def extract_conllu_data(filename: str, feature: str, sentences: bool = True):
+def extract_conllu_data(filename: str, feature: str, sentences: bool = True, combined: bool = False):
     '''A function that allows for the extraction of the desired data from a conllu file, structured into sentences or not.
     
     Args:
         filename (str): The name of the .conllu file to be read.
         feature (str): The name of the desired conllu format feature.
         sentences (bool): Whether or not the output should be a list of lists of strings representing words in separate sentences.
+        combined (bool): Whether or not the tokens and tags should be returned in one list of space-separated elements.
         
     Returns:
         A list of the original tokens (tokenized sentences), a list of the corresponding features, and a list of full original 
@@ -23,17 +24,22 @@ def extract_conllu_data(filename: str, feature: str, sentences: bool = True):
         return
     
     # specifying lists
+    tokens_features = []
     tokens = []
     features = []
     data = []
     
+    # opening the file
     with open(filename) as f:
         text = f.read()
     
+    # parsing the file
     sents = conllu.parse(text)
     
+    # selecting the relevant data and adding it to relevant lists
     for sentence in sents:
         data.append(sentence.metadata['text'])
+        sent_tokens_features = []
         sent_tokens = []
         sent_features = []
         for entry in sentence:
@@ -43,15 +49,27 @@ def extract_conllu_data(filename: str, feature: str, sentences: bool = True):
             sent_tokens.append(token)
             sent_features.append(feat)
             
+            if combined:  # this will return a different data structure
+                sent_tokens_features.append(' '.join([token, feat]))
+            
             
         tokens.append(sent_tokens)
         features.append(sent_features)
-            
+        
+        if combined:
+            tokens_features.append(sent_tokens_features)
+    
+    # unravelling the sentence-level lists if needed
     if not sentences:
         tokens = [x for sentence in tokens for x in sentence]
         features = [x for sentence in features for x in sentence]
-        
-    return tokens, features, data
+        if combined:
+            tokens_features = [x for sentence in tokens_features for x in sentence]
+            
+    if combined:
+        return tokens_features, data
+    else:        
+        return tokens, features, data
 
 def get_measures(gold_standard: list, predictions: list, labels: list = [], matrix: bool = False):
     '''A function intended for retrieving a selection of evaluation measures for comparing the gold standard and the tagger
@@ -62,10 +80,14 @@ def get_measures(gold_standard: list, predictions: list, labels: list = [], matr
     Args:
         gold_standard (list[str]): A list of gold standard labels.
         predictions (list[str]): A list of predicted labels.
+        labels (list[str]): A list of labels (if it needs to be specified).
+        matrix (bool): Whether or not to produce a confusion matrix.
     '''
-    if labels == []:
+    
+    if labels == []:  # setting up a list of labels based on the training data
         labels = sorted(list(set(gold_standard)))
 
+    # printing out the measures
     print('MEASURES:')
     print(f'Accuracy: {"{:.2%}".format(sklearn.metrics.accuracy_score(gold_standard, predictions))}')
     print(f'Matthew\'s Correlation Coefficient: {"{:.2%}".format(sklearn.metrics.matthews_corrcoef(gold_standard, predictions))}')
@@ -80,7 +102,8 @@ def get_measures(gold_standard: list, predictions: list, labels: list = [], matr
     for i in range(0,len(labels)):
         print(f'\t{labels[i]}: {"{:.2%}".format(recall[i])}')
     print()
-
+    
+    # printing out and saving the confusion matrix
     if matrix:
         print('Confusion matrix:')
         cm = sklearn.metrics.confusion_matrix(gold_standard, predictions)
